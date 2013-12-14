548132d0989133e6704b12c6a14470f7569a3aeb
alK13 <webpetit@gmail.com>
2013-12-14 13:35:09 +0100

    Adds meta viewport to enable responsive.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
     <head <?php language_attributes(); ?>>
         <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php wp_bootstrap_title(); ?> | <?php bloginfo('name'); ?></title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
         <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/js/jquery-1.10.2.min.js"></script>
