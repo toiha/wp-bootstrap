65aec57abece939c933a999546c723ec508902f2
alK13 <webpetit@gmail.com>
2013-12-16 20:59:00 +0100

    Fixes the footer content.

--- footer.php
+++ footer.php
@@ -2,10 +2,13 @@
         </div>
         <footer id="footer">
             <div class="container">
-                <p>Propulsé par <a href="http://wordpress.org">WordPress</a>
-                    - <a href="http://getbootstrap.com/">Bootstrap 3</a>  
+                <p>
+                    <a href="http://developathe.com">Dévelopathe</a>  
                     - <a href="https://github.com/developathe/wp-bootstrap">GitHub</a>  
-                    - Author : <a href="http://al.k13.fr">_alK13</p>
+                    - Author : <a href="http://al.k13.fr">_alK13</a>
+                    - Propulsé par <a href="http://wordpress.org">WordPress</a>
+                    - <a href="http://getbootstrap.com/">Bootstrap 3</a>
+                </p>
             </div>
         </footer>
         <?php wp_footer(); ?>
