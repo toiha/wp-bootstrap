e1d889e6b5f61c1bdd209d68148561c979334643
Tali-Adebisi <n.talichet@gmail.com>
2013-12-19 04:18:12 +0100

    Adds "Tali Adebisi" in footer.

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
                 <p>
                     <a href="http://developathe.com">Dévelopathe</a>  
                     - <a href="https://github.com/developathe/wp-bootstrap">GitHub</a>  
-                    - Author : <a href="http://al.k13.fr">_alK13</a>
+                    - Author : <a href="http://al.k13.fr">_alK13</a> & <a href="http://tali-blog.fr">Tali Adebisi</a>
                     - Propulsé par <a href="http://wordpress.org">WordPress</a>
                     - <a href="http://getbootstrap.com/">Bootstrap 3</a>
                 </p>
